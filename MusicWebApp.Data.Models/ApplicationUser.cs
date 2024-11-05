088f8e7a7032aeb31b74fb44859548e039e43b8f
nikicha2209 <nikicha08@gmail.com>
2024-11-05 20:31:53 +0200

    Created model ApplicationUser

---
+++ MusicWebApp.Data.Models/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MusicWebApp.Data.Models
+{
+    public class ApplicationUser : IdentityUser<Guid>
+    {
+        public ApplicationUser()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public TimeSpan ListeningTime { get; set; } = TimeSpan.Zero;
+        public int Xp { get; set; }
+    }
+}

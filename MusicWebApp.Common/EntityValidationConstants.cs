0c9a5225241845a9a61cdfc2f54ee0bb5efa7027
nikicha2209 <nikicha08@gmail.com>
2024-11-05 19:41:19 +0200

    Created model Song and added constraints for its properties

--- MusicWebApp.Common/EntityValidationConstants.cs
+++ MusicWebApp.Common/EntityValidationConstants.cs
@@ -8,10 +8,20 @@
             public const int NameMaxLength = 30;
 
             public const int DescriptionMinLength = 30;
-            public const int DescriptionMaxLength = 5000;
+            public const int DescriptionMaxLength = 2000;
 
             public const int ImageUrlMinLength = 5;
             public const int ImageUrlMaxLength = 70;
+        }
+
+        public static class Song
+        {
+            public const int TitleMinLength = 3;
+            public const int TitleMaxLength = 200;
+
+            public const string DurationMinValue = "00:00:01";
+            public const string DurationMaxValue = "24:00:00";
+
 
         }
     }
---
+++ MusicWebApp.Data.Models/Song.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+using static MusicWebApp.Common.EntityValidationConstants.Song;
+
+namespace MusicWebApp.Data.Models
+{
+    public class Song
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        [MaxLength(TitleMaxLength)]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        [Range(typeof(TimeSpan),DurationMinValue, DurationMaxValue)]
+        public TimeSpan Duration { get; set; } = TimeSpan.Zero;
+
+
+        [Required]
+        [ForeignKey(nameof(ArtistId))]
+        public Artist Artist { get; set; } = null!;
+
+        [Required]
+        public Guid ArtistId { get; set; }
+
+
+        [ForeignKey(nameof(AlbumId))]
+        public Album? Album { get; set; }
+
+        [Required]
+        public Guid? AlbumId { get; set; } //it's possible for a song to not be part of any album
+    }
+}

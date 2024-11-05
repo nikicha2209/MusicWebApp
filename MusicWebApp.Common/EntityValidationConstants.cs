046fb4f29f0609036e7e2115219f8c6a07cbfa67
nikicha2209 <nikicha08@gmail.com>
2024-11-05 19:23:25 +0200

    Created model Artist and added constraints for its properties

---
+++ MusicWebApp.Common/EntityValidationConstants.cs
@@ -0,0 +1,18 @@
+﻿namespace MusicWebApp.Common
+{
+    public static class EntityValidationConstants
+    {
+        public static class Artist
+        {
+            public const int NameMinLength = 3;
+            public const int NameMaxLength = 30;
+
+            public const int DescriptionMinLength = 30;
+            public const int DescriptionMaxLength = 5000;
+
+            public const int ImageUrlMinLength = 5;
+            public const int ImageUrlMaxLength = 70;
+
+        }
+    }
+}
---
+++ MusicWebApp.Data.Models/Artist.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Net.Mime;
+using static MusicWebApp.Common.EntityValidationConstants.Artist;
+
+namespace MusicWebApp.Data.Models
+{
+    public class Artist
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        [MaxLength(NameMaxLength)]
+        public string Name { get; set; } = null!;
+
+        [MaxLength(DescriptionMaxLength)]
+        public string? Description { get; set; }
+
+        [MaxLength(ImageUrlMaxLength)]
+        public string? ImageUrl { get; set; }
+        public virtual ICollection<Song> Songs { get; set; } = new HashSet<Song>();
+        public virtual ICollection<Album> Albums { get; set; } = new HashSet<Album>();
+    }
+}

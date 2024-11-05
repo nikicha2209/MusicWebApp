a8de2f57d68fcc99c3f6b0268a20fb5a789a1ede
nikicha2209 <nikicha08@gmail.com>
2024-11-05 20:50:54 +0200

    Created and configurated the DB context

---
+++ MusicWebApp.Data/MusicDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MusicWebApp.Data.Models;
+
+namespace MusicWebApp.Data
+{
+    public class MusicDbContext : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>
+    {
+        public MusicDbContext(DbContextOptions<MusicDbContext> options)
+            :base(options)
+        {
+        }
+
+        public MusicDbContext()
+        {
+            
+        }
+
+        public virtual DbSet<Artist> Artists { get; set; } = null!;
+        public virtual DbSet<Song> Songs { get; set; } = null!;
+        public virtual DbSet<Album> Albums { get; set; } = null!;
+
+    }
+}
